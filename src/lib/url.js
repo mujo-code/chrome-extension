9467d7405f8d04e14c66a697694881ea04fa4b59
Jacob Lowe <jacoblowe2.0@gmail.com>
2019-06-28 17:49:55 -0700

    support punycode in url for mujō.com (#17)

--- src/lib/url.js
+++ src/lib/url.js
@@ -1,5 +1,7 @@
+import punycode from 'punycode'
+
 export const parse = url => new URL(url)
-export const shortURL = url => parse(url).hostname
+export const shortURL = url => punycode.toUnicode(parse(url).hostname)
 export const origin = url => parse(url).origin
 
 export const queryParams = url => {
