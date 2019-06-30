4bba9612e4ceb256c865e8a403c27e97e2d3d5d3
Jacob Lowe <jacoblowe2.0@gmail.com>
2019-06-30 12:35:25 -0700

    Not enough data placeholder (#19)
    * add in not enough data icon and test
    
    * change copy a bit

--- src/components/screen-time/index.js
+++ src/components/screen-time/index.js
@@ -5,6 +5,7 @@ import { siteTimeToChartData } from '../../lib/aggregation'
 import { HeaderS } from '../fonts'
 import { Graph } from '../graph'
 import { Modal } from './modal'
+import { NotEnoughData, hasEnoughData } from './not-enough-data'
 import { reduceSegmentToUrls } from './reducer'
 
 export const ScreenTime = ({
@@ -16,6 +17,7 @@ export const ScreenTime = ({
   const graphData = siteTimeToChartData(data)
   const theme = useTheme()
   const { foreground, highlight } = theme
+  const showGraph = hasEnoughData(graphData, data)
   return (
     <Box
       flex="1"
@@ -28,22 +30,26 @@ export const ScreenTime = ({
       layer="1"
     >
       <HeaderS color={foreground}>Screen Time</HeaderS>
-      <Graph
-        data={graphData}
-        width={600}
-        height={275}
-        strokeWidth={16}
-        textFill={foreground}
-        stroke={highlight}
-        spacingAngle={16}
-        strokeLinecap="round"
-        radius={100}
-        selected={selectedSegment}
-        selectedStroke={foreground}
-        onSegmentClick={seg =>
-          setSelectedSegment(reduceSegmentToUrls(seg))
-        }
-      />
+      {showGraph ? (
+        <Graph
+          data={graphData}
+          width={600}
+          height={275}
+          strokeWidth={16}
+          textFill={foreground}
+          stroke={highlight}
+          spacingAngle={16}
+          strokeLinecap="round"
+          radius={100}
+          selected={selectedSegment}
+          selectedStroke={foreground}
+          onSegmentClick={seg =>
+            setSelectedSegment(reduceSegmentToUrls(seg))
+          }
+        />
+      ) : (
+        <NotEnoughData />
+      )}
       {selectedSegment ? (
         <Modal
           theme={theme}
