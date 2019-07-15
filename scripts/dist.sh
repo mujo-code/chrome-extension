cd6f643764ee18c5ba684b3a9fe38e98ed9d3456
Jacob Lowe <jacoblowe2.0@gmail.com>
2019-07-15 14:05:02 -0700

    Beta DIST (#32)
    * add in beta  build to scripts
    
    * add in beta asset and script to decorate manifest file
    
    * add in sub details

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,15 +1,16 @@
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
-
-PACKAGE_NAME=$(cat public/manifest.json \
+PACKAGE_NAME=$(cat build/manifest.json \
   | grep short_name \
   | head -1 \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
 
-npm run build && zip "$PACKAGE_NAME-$PACKAGE_VERSION.zip" -r build
+PACKAGE_VERSION=$(cat build/manifest.json \
+  | grep "\"version" \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+
+zip "$PACKAGE_NAME-$PACKAGE_VERSION.zip" -r build
