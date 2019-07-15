1998168b482d4c72b4c80cd4c11d166182a3c35e
Jacob Lowe <jacoblowe2.0@gmail.com>
2019-07-15 14:08:49 -0700

    Purchase fail tracking and alerting. (#33)
    * add in try catch around payments to send out exceptiion tracking
    
    * add
    
    * add in better callstack
    
    * tracking payment success

--- src/lib/payment.js
+++ src/lib/payment.js
@@ -1,4 +1,6 @@
 import { promisifyOptions } from './promisify'
+import { exception, track } from './tracker'
+import { set } from './util'
 
 const env = process.env.PRODUCT_ENV || 'prod'
 // TODO make this less scarey
@@ -21,11 +23,35 @@ export const getPurchases = async () => {
   return response.response.details
 }
 
-export const buy = sku => {
+export const buy = async sku => {
   const parameters = { env }
   const options = {
     parameters,
     sku,
   }
-  return promisifyOptions(paymentsAPI().buy, options)
+  // create callstack outside catch
+  const { stack } = new Error()
+  try {
+    const ret = await promisifyOptions(paymentsAPI().buy, options)
+    track({
+      category: 'payment',
+      action: 'success',
+      label: sku,
+    })
+    return ret
+  } catch (e) {
+    // Logging error
+    const type = e.response.errorType
+    const paymentError = new Error(
+      `Failed to purchase subscription [${type})]`
+    )
+    set(paymentError, 'stack', stack)
+    exception(paymentError)
+    track({
+      category: 'payment',
+      action: 'error',
+      label: paymentError.message,
+    })
+    throw paymentError
+  }
 }
