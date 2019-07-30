78375c46bb3447e2602540e7e8f20a1fd31efc9f
jcblw <578259+jcblw@users.noreply.github.com>
2019-07-30 12:33:10 -0700

    add in some simple configurability

--- src/components/button-group/index.js
+++ src/components/button-group/index.js
@@ -3,13 +3,19 @@ import { removeKeys } from '@jcblw/box/dist/lib/remove-keys'
 import React from 'react'
 import { Button } from '../button'
 
+const defaultSpacerStyles = { width: '1px' }
+
 export const ButtonGroup = props => {
+  const {
+    spacerStyles = defaultSpacerStyles,
+    childComponent = Button,
+  } = props
   const childArr = React.Children.toArray(props.children)
-  const otherProps = removeKeys(props, 'children')
+  const otherProps = removeKeys(props, 'children', 'spacerStyles')
   return (
     <Box display="flex" direction="row" {...otherProps}>
       {childArr
-        .filter(child => child.type === Button)
+        .filter(child => child.type === childComponent)
         .map((child, i, arr) => {
           const isFirst = i === 0
           const isLast = i === arr.length - 1
@@ -27,7 +33,7 @@ export const ButtonGroup = props => {
           return (
             <React.Fragment key={`el-${i}`}>
               {element}
-              {isLast ? null : <Box css={{ width: '1px' }} />}
+              {isLast ? null : <Box css={spacerStyles} />}
             </React.Fragment>
           )
         })}
