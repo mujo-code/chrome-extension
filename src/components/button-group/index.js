d34fe1c3d0b98111513b8ac0643929ac5831b9fa
jcblw <578259+jcblw@users.noreply.github.com>
2019-07-30 11:46:32 -0700

    add in Button filter and also a snapshot

--- src/components/button-group/index.js
+++ src/components/button-group/index.js
@@ -1,36 +1,36 @@
 import { Box } from '@jcblw/box'
 import { removeKeys } from '@jcblw/box/dist/lib/remove-keys'
 import React from 'react'
-
-// clone element pass props but add new props.
+import { Button } from '../button'
 
 export const ButtonGroup = props => {
   const childArr = React.Children.toArray(props.children)
   const otherProps = removeKeys(props, 'children')
   return (
     <Box display="flex" direction="row" {...otherProps}>
-      {childArr.map((child, i, arr) => {
-        const isFirst = i === 0
-        const isLast = i === arr.length - 1
-        const borderTopLeftRadius = isFirst ? 'l' : 'none'
-        const borderBottomLeftRadius = isFirst ? 'l' : 'none'
-        const borderTopRightRadius = isLast ? 'l' : 'none'
-        const borderBottomRightRadius = isLast ? 'l' : 'none'
-        const element = React.cloneElement(child, {
-          key: `el-${i}`,
-          borderRadius: 'none',
-          borderTopLeftRadius,
-          borderBottomLeftRadius,
-          borderTopRightRadius,
-          borderBottomRightRadius,
-        })
-        return (
-          <>
-            {element}
-            {isLast ? null : <Box css={{ width: '1px' }} />}
-          </>
-        )
-      })}
+      {childArr
+        .filter(child => child.type === Button)
+        .map((child, i, arr) => {
+          const isFirst = i === 0
+          const isLast = i === arr.length - 1
+          const borderTopLeftRadius = isFirst ? 'l' : 'none'
+          const borderBottomLeftRadius = isFirst ? 'l' : 'none'
+          const borderTopRightRadius = isLast ? 'l' : 'none'
+          const borderBottomRightRadius = isLast ? 'l' : 'none'
+          const element = React.cloneElement(child, {
+            borderRadius: 'none',
+            borderTopLeftRadius,
+            borderBottomLeftRadius,
+            borderTopRightRadius,
+            borderBottomRightRadius,
+          })
+          return (
+            <React.Fragment key={`el-${i}`}>
+              {element}
+              {isLast ? null : <Box css={{ width: '1px' }} />}
+            </React.Fragment>
+          )
+        })}
     </Box>
   )
 }
