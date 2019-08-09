4fb022667b965cfefb87dfab30fc339e8212a12b
jcblw <578259+jcblw@users.noreply.github.com>
2019-08-09 16:18:55 -0700

    and in fix for background test

--- src/background.test.js
+++ src/background.test.js
@@ -1,23 +1,26 @@
-/* eslint-disable-next-line import-order-alphabetical/order */
+/* eslint-disable import-order-alphabetical/order */
 import {
   onMessage,
   alarms,
   webNavigation,
   notifications,
 } from './lib/extension'
+import { wait } from './lib/async-helpers'
 
 jest.mock('./background/alarm')
 jest.mock('./background/tracking')
-const { addHeartBeat } = require('./background/alarm')
+const { initAlarms } = require('./background/alarm')
 const { injectTracking } = require('./background/tracking')
 
-test('all the automatic setup of background', () => {
+test('all the automatic setup of background', async () => {
   /* eslint-disable-next-line global-require */
   require('./background')
   expect(onMessage).toBeCalled()
   expect(alarms.onAlarm.addListener).toBeCalled()
   expect(webNavigation.onCommitted.addListener).toBeCalled()
   expect(notifications.onClicked.addListener).toBeCalled()
-  expect(addHeartBeat).toBeCalled()
   expect(injectTracking).toBeCalled()
+  await wait(400)
+  // happens in a promise chain now so its deferred
+  expect(initAlarms).toBeCalled()
 })
