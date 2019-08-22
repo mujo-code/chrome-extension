5ab424b2da396dad0d7dca7dbafc22c30dbba98d
jcblw <578259+jcblw@users.noreply.github.com>
2019-08-22 07:49:12 -0700

    add in some more config start index file and hooks dir

--- packages/mujo-plugin/src/components/plugin-provider.js
+++ packages/mujo-plugin/src/components/plugin-provider.js
@@ -9,8 +9,13 @@ const { Provider } = context
   This provider is going to accept some common tool inside of the
   extensions to allow the plugins to have access to common functionality
   within the app with out exposing it to window or something else gross
+
+  env       - the environment in the extension (background | content | ntp)
+  extension - sugar sytax extension of browser api's
+  storage   - access to the database
+  config    - configuration for the extension
 */
 
-export const PluginProvider = ({ env, extension, storage }) => {
-  return <Provider value={{ env, extension, storage }}></Provider>
-}
+export const PluginProvider = ({ env, extension, storage, config }) => (
+  <Provider value={{ env, extension, storage, config }}></Provider>
+)
