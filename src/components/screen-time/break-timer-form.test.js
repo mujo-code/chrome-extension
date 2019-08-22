756b92a4d2a5846d1afcc31481a1c37020ad7096
Temmyogunbo <Ogunbotemilola@yahoo.com>
2019-08-22 16:56:33 +0100

    add test

--- src/components/screen-time/break-timer-form.test.js
+++ src/components/screen-time/break-timer-form.test.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { create } from 'react-test-renderer'
+import { minutesToMS } from '../../lib/time'
+import { Input } from '../input'
 import { BreakTimerForm } from './break-timer-form'
 
 test('BreakTimerForm should match snapshot', () => {
@@ -10,4 +12,23 @@ test('BreakTimerForm should match snapshot', () => {
 test('BreakTimerForm should match snapshot when enabled', () => {
   const tree = create(<BreakTimerForm enabled={true} />).toJSON()
   expect(tree).toMatchSnapshot()
+})
+
+test('BreakerTimerForm input should be positive', () => {
+  const fn = jest.fn()
+  const originalURL = 'foo'
+  const enabled = true
+  const wrapper = create(
+    <BreakTimerForm
+      originalURL={originalURL}
+      setBreakTimer={fn}
+      enabled={enabled}
+    />
+  )
+  const { root } = wrapper
+  const input = root.findByType(Input)
+
+  input.props.onChange({ target: { value: -11 } })
+
+  expect(fn).toBeCalledWith(originalURL, minutesToMS(11), enabled)
 })
