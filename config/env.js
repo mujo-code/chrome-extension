b337ce3e67ba70dec3b70089cf190c16a92628b9
jcblw <578259+jcblw@users.noreply.github.com>
2019-09-07 15:07:39 -0700

    add in fix for script

--- config/env.js
+++ config/env.js
@@ -75,7 +75,7 @@ function getClientEnvironment(publicUrl) {
         // images into the `src` and `import` them in code to get their paths.
         PUBLIC_URL: publicUrl,
         SENTRY_DSN: process.env.SENTRY_DSN,
-        VESION: pkg.version,
+        VERSION: pkg.version,
         UA: process.env.UA,
       }
     )
