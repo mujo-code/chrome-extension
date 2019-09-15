d4b7e7f7df94069320b1eed42f92fd3b3de66cf8
jcblw <578259+jcblw@users.noreply.github.com>
2019-09-15 15:04:05 -0700

    add in version of the plugins

--- src/components/plugin-provider/index.js
+++ src/components/plugin-provider/index.js
@@ -6,13 +6,22 @@ import * as extension from '../../lib/extension'
 
 // NOTE we use a proxy to allow for access to `useExtension`
 export const NTPPluginProvider = ({ children }) => {
-  const { pushTab, currentTab, removeTab } = useExtension()
+  const {
+    pushTab,
+    currentTab,
+    removeTab,
+    pushSetting,
+    removeSetting,
+    updateSetting,
+  } = useExtension()
   const tabs = { pushTab, removeTab, currentTab }
+  const settings = { pushSetting, removeSetting, updateSetting }
   const props = {
     env: 'ntp',
     extension,
     constants,
     tabs,
+    settings,
   }
   return <Provider {...props}>{children}</Provider>
 }
