8b7064e17670bdfd1b96b487709dc4fea410282e
jcblw <578259+jcblw@users.noreply.github.com>
2019-09-16 08:16:19 -0700

    add in new modified hook to work on background and cliients

--- packages/mujo-plugin/src/hooks/use-storage/index.js
+++ packages/mujo-plugin/src/hooks/use-storage/index.js
@@ -1,1 +1,11 @@
-export { useStorageBackground as useStorage } from './background'
+import { useContext } from 'react'
+import { useStorageBackground } from './background'
+import { useStorageClient } from './client'
+
+export const useStorage = (...args) => {
+  const { env } = useContext(context)
+  // NOTE: conditional hook, but should not switch on renders since environment
+  // should not change on renders
+  const useHook = env === 'background' ? useStorageBackground : useStorageClient
+  return useHook(...args)
+}
