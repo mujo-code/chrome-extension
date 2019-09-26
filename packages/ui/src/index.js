4f725a351411401e7a416b3fa4cbcde84bf80ba3
jcblw <578259+jcblw@users.noreply.github.com>
2019-09-26 08:03:49 -0700

    exporting out with styles  injected

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -1,3 +1,7 @@
+import { styleGuide } from '@mujo/box'
+import * as utilStyles from './styles/utils'
+
+styleGuide.push(utilStyles)
 /* barrel files till we can find a easier pattern */
 export * from './components'
 export * from './styles/colors'
