d6409ed20917cd71415d732b6f2252371f945b05
Jacob Lowe <jacoblowe2.0@gmail.com>
2019-10-20 19:46:38 -0700

    add in fix to bad deeplink (#264)

--- packages/extension/src/hooks/use-extension/use-deeplink.js
+++ packages/extension/src/hooks/use-extension/use-deeplink.js
@@ -1,7 +1,7 @@
 import { Url } from '@mujo/utils'
 import { useEffect } from 'react'
 
-const { queryParams, shortURL, origin } = Url
+const { queryParams } = Url
 
 export const useDeeplink = ({
   appReady,
@@ -10,15 +10,9 @@ export const useDeeplink = ({
 }) => {
   useEffect(() => {
     if (!appReady) {
-      const { site, play } = queryParams(window.location.href)
+      const { play } = queryParams(window.location.href)
       if (play) {
         setPlayerIsOpen(true)
-      } else if (site) {
-        setSelectedSegment({
-          label: shortURL(site),
-          urls: [origin(site)],
-          link: site,
-        })
       }
     }
   }, [appReady, setPlayerIsOpen, setSelectedSegment])
