31588f67a000d99d395503603bf035f8cc2ac67a
Jacob Lowe <jacoblowe2.0@gmail.com>
2020-06-11 08:23:07 -0700

    reverting some changes to babel

--- config/babel-jest.transformer.js
+++ config/babel-jest.transformer.js
@@ -1,6 +1,6 @@
 const babelJest = require('babel-jest')
 
 module.exports = babelJest.createTransformer({
-  presets: ['react-app', '@babel/preset-env'],
+  presets: ['react-app'],
   plugins: ['@babel/plugin-proposal-export-namespace-from'],
 })
